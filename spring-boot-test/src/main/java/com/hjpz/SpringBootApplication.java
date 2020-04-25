f7f68cc6486a14a4c4abf5065febf770ab8b2174
linjie <jm123!@#>
2020-04-25 13:24:08 +0800

    SpringBoot项目提交

---
+++ spring-boot-test/src/main/java/com/hjpz/SpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.hjpz;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @ClassName : SpringBootApplication
+ * @Description : SpringBoot快速启动器
+ * @Author : 林杰
+ * @Date: 2020-04-25 12:58
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
